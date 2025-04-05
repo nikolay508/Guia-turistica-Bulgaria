1fe6a0fd8d13d24f68f7957705407f1a13409990
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2025-04-05 19:30:37 +0300

    Revert "joke"
    This reverts commit 87af226ad363e069d97bb9296389febeb8cba378.

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,52 +1,31 @@
 import { cardData } from "../constants/cardData.jsx";
 import { FeatureCard } from "../components/FeatureCard.jsx";
-import { Toaster, toast } from "react-hot-toast";
-import { PopUp } from "../components/pop-up.jsx";
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-green-50 to-blue-50 py-25 px-4">
-      <div>
-        <Toaster />
-      </div>
       <div className="max-w-6xl mx-auto relative">
         <div className="text-center mb-10 relative">
           <h1 className="text-5xl font-bold text-green-800">
             Guía turística: Bulgaria
           </h1>
-          <img
-            src="./bg.png"
-            alt="Map of Bulgaria"
-            className="absolute left-272 top-5"
-            style={{ maxWidth: "150px", transform: "rotate(345deg)" }}
-          />
-          <img
-            src="./bg-slogan.png"
-            alt="Map of Bulgaria"
-            className="absolute right-272 top-5"
-            style={{ maxWidth: "140px", transform: "rotate(30deg)" }}
-          />
+          <img src="./bg.png" alt="Map of Bulgaria" className="absolute left-272 top-5" style={{ maxWidth: '150px', transform: 'rotate(345deg)' }} />
+          <img src="./bg-slogan.png" alt="Map of Bulgaria" className="absolute right-272 top-5" style={{ maxWidth: '140px', transform: 'rotate(30deg)' }} />
         </div>
         <div className="text-center mb-16">
           <p className="text-xl text-gray-600 max-w-4xl mx-auto">
-            <br />
-            Bulgaria es un país fascinante del sureste de Europa, con montañas,
-            playas, pueblos antiguos y ciudades modernas. Su rica historia y
-            cultura reflejan influencias tracias, romanas, otomanas y ortodoxas.
-            <br />
-            <br />
-            Es un destino ideal para quienes buscan naturaleza, historia y buena
-            comida, además de ser uno de los países más económicos de Europa.
+            <br/>Bulgaria es un país fascinante del sureste de Europa, con montañas, playas, pueblos antiguos y ciudades modernas. Su rica historia y cultura reflejan influencias tracias, romanas, otomanas y ortodoxas.
+            <br/><br/>Es un destino ideal para quienes buscan naturaleza, historia y buena comida, además de ser uno de los países más económicos de Europa. 
             Aquí tienes una guía con todo lo necesario para tu viaje.
           </p>
         </div>
 
         <div className="grid md:grid-cols-3 gap-8 mb-16 md:justify-items-center">
           {cardData.map((card, index) => (
-            <FeatureCard
-              key={index}
-              icon={card.icon}
-              title={card.title}
+            <FeatureCard 
+              key={index} 
+              icon={card.icon} 
+              title={card.title} 
               description={card.description}
             />
           ))}
@@ -56,4 +35,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Home; 
